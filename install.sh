862cdde1e9146c61c7de4944b66271c6a0388e46
Hirusha Adikari <hirushaadi@gmail.com>
2024-10-23 01:33:32 -0400

    clean

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 dir="$HOME/"
-# dir="./tmp/"
 
 # colors
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # reset
-
 
 download_file() {
     echo -e "${YELLOW}Starting to download: $1${NC}"
